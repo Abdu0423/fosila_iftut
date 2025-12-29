a05a784449382bcdc1f1a297052b7e72bed165f1
Abdu0423 <rogman_94@mail.ru>
2025-12-29 18:10:30 +0500

    ;';l'

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -103,10 +103,22 @@ class SmsService
             // Нормализуем номер телефона для API (убираем + и оставляем только цифры)
             $phoneNumber = preg_replace('/[^0-9]/', '', $phone);
             
-            // Вычисляем str_hash согласно документации OsonSMS:
-            // SHA256(txn_id + login + from + phone_number + pass_salt_hash)
-            $hashString = $txnId . $this->login . $this->sender . $phoneNumber . $this->hash;
+            // Вычисляем str_hash согласно документации OsonSMS
+            // Формат: SHA256(txn_id;login;pass_salt_hash)
+            // где pass_salt_hash - это значение из конфига 'hash'
+            $hashString = $txnId . ';' . $this->login . ';' . $this->hash;
             $strHash = hash('sha256', $hashString);
+            
+            // Логируем для отладки
+            if ($this->loggingEnabled) {
+                Log::channel(config('sms.logging.channel'))->info('OsonSMS hash calculation', [
+                    'txn_id' => $txnId,
+                    'login' => $this->login,
+                    'pass_salt_hash' => $this->hash,
+                    'hash_string' => $hashString,
+                    'str_hash' => $strHash,
+                ]);
+            }
             
             $response = Http::get($this->server, [
                 'login' => $this->login,
