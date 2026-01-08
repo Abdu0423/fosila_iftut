f18a118ef8f161cc22d4dfd7396ff497e7de6d6f
Abdu0423 <rogman_94@mail.ru>
2026-01-08 10:15:18 +0500

    ываыва

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -441,11 +441,7 @@ const menuItems = computed(() => {
     case 'education_department':
       return [
         { title: t('navigation.dashboard'), icon: 'mdi-view-dashboard', route: '/education' },
-        { title: t('education_department.users_menu'), icon: 'mdi-account-group', route: '/education/users' },
-        { title: t('education_department.groups_menu'), icon: 'mdi-account-multiple', route: '/education/groups' },
         { title: t('education_department.subjects_menu'), icon: 'mdi-book-open-page-variant', route: '/education/subjects' },
-        { title: t('education_department.departments_menu'), icon: 'mdi-office-building', route: '/education/departments' },
-        { title: t('education_department.specialties_menu'), icon: 'mdi-school', route: '/education/specialties' },
         { title: t('education_department.schedules_menu'), icon: 'mdi-calendar-clock', route: '/education/schedules' },
         { title: t('navigation.my_lessons'), icon: 'mdi-teach', route: '/education/lessons' },
         { title: t('navigation.my_tests'), icon: 'mdi-help-circle', route: '/education/tests' },
